44eb3eb1225ee33642396deb6513c10be6f0c927
haruna0220 <haruna.donishi@icloud.com>
2024-09-20 20:51:23 +0900

    ログインページ

---
+++ frontend/src/loginPage/login.jsx
@@ -0,0 +1,14 @@
+// src/Login.js
+// import React, { useState } from 'react';
+import './Login.css';
+
+const Login = () => {
+  return (
+    <div className="login-container">
+      <h2 id='ログイン文字'>Go to はなまるレース</h2>
+        <button className='submitButton' type="submit">GitHubログイン画面へ移動</button>
+    </div>
+  );
+};
+
+export default Login;
